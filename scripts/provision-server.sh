70de56070b0004b695faa2deb10271b0f3c62d8a
darryl west <darryl.west@raincitysoftware.com>
2025-08-02 07:07:20 -0700

    valkey install, setup and maintenance plans

--- scripts/provision-server.sh
+++ scripts/provision-server.sh
@@ -135,7 +135,7 @@ install_dev_tools() {
     # pull in just what is needed
     echo "--- Task: Installing core development tools (dropping out here ---"
 
-    apt install -y make xz-utils vim neovim fswatch openssl libssl-dev jq lcov btop unzip
+    apt install -y make xz-utils vim neovim fswatch openssl libssl-dev jq lcov btop gnupg unzip
 
     mark_as_done "$task_name"
 
